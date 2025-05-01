e7cb5f138ae821a4b1aeee7b42b0cddec27d9536
Sitong <nickren93@gmail.com>
2025-05-01 00:38:00 -0400

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    #pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        '''prints "size must be an integer" if size is not an integer.'''
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    
